6334465f681707a14c2488eb044a9b70f14d7803
shakhovrs <rs.shakhov@gmail.com>
2023-09-16 02:08:29 +0700

    fsdfsd

--- Src/CryptoChat.Api.Contracts/WebRoutes/RoomControllerWebRoutes.cs
+++ Src/CryptoChat.Api.Contracts/WebRoutes/RoomControllerWebRoutes.cs
@@ -4,6 +4,6 @@ public static class RoomControllerWebRoutes
 {
     public const string BasePath = "api/rooms";
     public const string GetMessagesFromRoom = "{roomId:int}/messages";
-    public const string CreateRoom = "";
+    public const string CreateRoom = "create-room";
     public const string SendMessageToRoom = "{roomId:int}/send-message";
 }
