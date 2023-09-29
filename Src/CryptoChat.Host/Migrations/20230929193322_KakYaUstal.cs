063dd36fc1a062659bd111149ec17a2098b37b1b
shakhovrs <rs.shakhov@gmail.com>
2023-09-30 05:16:23 +0700

    hjkkhj

---
+++ Src/CryptoChat.Host/Migrations/20230929193322_KakYaUstal.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoChat.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class KakYaUstal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "room",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_room", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WarehouseItemCategories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarehouseItemCategories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    password = table.Column<string>(type: "TEXT", nullable: true),
+                    category_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_WarehouseItemCategories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "WarehouseItemCategories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WarehouseItems",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    count = table.Column<int>(type: "INTEGER", nullable: false),
+                    category_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WarehouseItems", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_WarehouseItems_WarehouseItemCategories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "WarehouseItemCategories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "message",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    text = table.Column<string>(type: "TEXT", nullable: true),
+                    user_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    room_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_message", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_message_room_room_id",
+                        column: x => x.room_id,
+                        principalTable: "room",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_message_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_room",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    user_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    room_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_room", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_room_room_room_id",
+                        column: x => x.room_id,
+                        principalTable: "room",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_user_room_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_message_room_id",
+                table: "message",
+                column: "room_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_message_user_id",
+                table: "message",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_category_id",
+                table: "user",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_name",
+                table: "user",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_room_room_id",
+                table: "user_room",
+                column: "room_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_room_user_id",
+                table: "user_room",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarehouseItems_category_id",
+                table: "WarehouseItems",
+                column: "category_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "message");
+
+            migrationBuilder.DropTable(
+                name: "user_room");
+
+            migrationBuilder.DropTable(
+                name: "WarehouseItems");
+
+            migrationBuilder.DropTable(
+                name: "room");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "WarehouseItemCategories");
+        }
+    }
+}
---
+++ Src/CryptoChat.Host/Migrations/20230929205734_AddFeatures.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoChat.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFeatures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FeatureToggles",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "TEXT", nullable: false),
+                    Enabled = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeatureToggles", x => x.Key);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FeatureToggles");
+        }
+    }
+}
